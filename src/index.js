f61123a560ecdaa94d2b98b1964b00cd30053555
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-17 05:08:48 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let submit = document.getElementById('submit')
+  let task = document.getElementById('new-task-description').value
+  let list = document.getElementById('list')
+  let ul = document.getElementById('tasks')
   submit.addEventListener("submit",(e) => {
     e.preventDefault();
-
+    let li = document.createElement('li')
+    li.innerHTML = task
+    ul.appendChild(li)
   })
 });

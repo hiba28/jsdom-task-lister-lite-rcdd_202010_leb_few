dc3cf1031050c8c70e05a397af914754e849363f
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-17 05:05:48 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let submit = document.getElementById('submit')
+  submit.addEventListener("submit",(e) => {
+    e.preventDefault();
+    
+  })
 });

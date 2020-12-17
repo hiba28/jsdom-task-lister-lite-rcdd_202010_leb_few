d30841fe906474128ce0db3f041dfd700576f762
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-17 05:11:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit",(e) => {
     e.preventDefault();
     let li = document.createElement('li')
-    li.innerHTML += task
+    li.inneText += task
     ul.appendChild(li)
   })
 });

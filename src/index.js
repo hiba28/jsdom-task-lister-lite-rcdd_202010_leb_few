249781b95f83d7a66978ce11386a61a7106ed7f7
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-17 05:12:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let form = document.getElementById('create-task-form')
-  let task = document.getElementById('new-task-description').value
+  let task = document.getElementById('new-task-description')
   let list = document.getElementById('list')
   let ul = document.getElementById('tasks')
 
   form.addEventListener("submit",(e) => {
     e.preventDefault();
     let li = document.createElement('li')
-    li.innerText += task
+    li.innerText = task
     li.style.color = "black"
     ul.appendChild(li)
   })
